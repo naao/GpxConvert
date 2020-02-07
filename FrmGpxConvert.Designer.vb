f989c3900aa5ca8a39954c23680a0781fb393ce3
naao <naao@mbg.nifty.com>
2020-02-07 20:19:43 +0900

    first commit ver0.1 to conbine Jog Note json file and gpx file

---
+++ FrmGpxConvert.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmGpxConvert
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LbxGpxFiles = New System.Windows.Forms.ListBox()
+        Me.LbxJsonFiles = New System.Windows.Forms.ListBox()
+        Me.LbxTcxFiles = New System.Windows.Forms.ListBox()
+        Me.TbxMessage = New System.Windows.Forms.TextBox()
+        Me.BtnExec = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'LbxGpxFiles
+        '
+        Me.LbxGpxFiles.FormattingEnabled = True
+        Me.LbxGpxFiles.ItemHeight = 12
+        Me.LbxGpxFiles.Location = New System.Drawing.Point(30, 215)
+        Me.LbxGpxFiles.Name = "LbxGpxFiles"
+        Me.LbxGpxFiles.Size = New System.Drawing.Size(182, 124)
+        Me.LbxGpxFiles.TabIndex = 0
+        '
+        'LbxJsonFiles
+        '
+        Me.LbxJsonFiles.FormattingEnabled = True
+        Me.LbxJsonFiles.ItemHeight = 12
+        Me.LbxJsonFiles.Location = New System.Drawing.Point(30, 62)
+        Me.LbxJsonFiles.Name = "LbxJsonFiles"
+        Me.LbxJsonFiles.Size = New System.Drawing.Size(182, 124)
+        Me.LbxJsonFiles.TabIndex = 1
+        '
+        'LbxTcxFiles
+        '
+        Me.LbxTcxFiles.FormattingEnabled = True
+        Me.LbxTcxFiles.ItemHeight = 12
+        Me.LbxTcxFiles.Location = New System.Drawing.Point(30, 361)
+        Me.LbxTcxFiles.Name = "LbxTcxFiles"
+        Me.LbxTcxFiles.Size = New System.Drawing.Size(182, 124)
+        Me.LbxTcxFiles.TabIndex = 2
+        '
+        'TbxMessage
+        '
+        Me.TbxMessage.Location = New System.Drawing.Point(258, 182)
+        Me.TbxMessage.Multiline = True
+        Me.TbxMessage.Name = "TbxMessage"
+        Me.TbxMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TbxMessage.Size = New System.Drawing.Size(261, 303)
+        Me.TbxMessage.TabIndex = 3
+        '
+        'BtnExec
+        '
+        Me.BtnExec.Location = New System.Drawing.Point(298, 48)
+        Me.BtnExec.Name = "BtnExec"
+        Me.BtnExec.Size = New System.Drawing.Size(158, 80)
+        Me.BtnExec.TabIndex = 4
+        Me.BtnExec.Text = "変換実行"
+        Me.BtnExec.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(30, 12)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Json"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 200)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(27, 12)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "GPX"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(15, 346)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 12)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "TCX出力"
+        '
+        'FrmGpxConvert
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(591, 504)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnExec)
+        Me.Controls.Add(Me.TbxMessage)
+        Me.Controls.Add(Me.LbxTcxFiles)
+        Me.Controls.Add(Me.LbxJsonFiles)
+        Me.Controls.Add(Me.LbxGpxFiles)
+        Me.Name = "FrmGpxConvert"
+        Me.Text = "GpxConvert"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LbxGpxFiles As ListBox
+    Friend WithEvents LbxJsonFiles As ListBox
+    Friend WithEvents LbxTcxFiles As ListBox
+    Friend WithEvents TbxMessage As TextBox
+    Friend WithEvents BtnExec As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ FrmGpxConvert.vb
@@ -0,0 +1,253 @@
+﻿Imports System.IO
+Imports System.Runtime.Serialization.Json
+Imports System.Text
+Imports System.Xml
+
+Public Class FrmGpxConvert
+    '変換用テンプレートファイルの場所・ファイル名を変更した場合は以下を編集
+    Friend strTemplateFileName As String = ".\template.tcx"
+    '読込みJSON／GPX／書出しTCXフォルダ名（ファイル群の場所）を変更した場合は以下を編集
+    Friend strJsonFolderName As String = ".\json.\"
+    Friend strGpxFolderName As String = ".\gpx.\"
+    Friend strTcxFolderName As String = ".\tcx_w.\"
+
+    Private Sub Execute()
+
+        'jsonフォルダ内の、.jsonファイル一覧を取得
+        Dim jsonFiles() = Directory.GetFiles(strJsonFolderName, "*.json", SearchOption.TopDirectoryOnly)
+        If jsonFiles.Count < 1 Then
+            Exit Sub 'jsonファイルが無い場合は何もしない
+        End If
+
+        TbxMessage.Text = Nothing
+
+        'jsonフォルダ内のjsonファイル群を1つずつ処理
+        For Each jsonFile As String In jsonFiles
+
+            'jsonファイルを読み込み、noteRootクラスのjsonRootオブジェクトに格納
+            Dim oJsonRoot As noteRoot = ReadJson(Of noteRoot)(jsonFile)
+
+            Dim strNoteDate As String = oJsonRoot.note.[date]
+            Dim strNoteDiary As String = oJsonRoot.note.diary
+
+            Dim strWorkoutMeter As String
+            Dim strWorkoutSec As String
+            Dim strGpxFilename As String
+
+            TbxMessage.Text += "[日付] " + strNoteDate + vbCrLf
+            TbxMessage.Text += "[日記] " + strNoteDiary + vbCrLf
+
+            'リストBOXへファイル名を追記
+            LbxJsonFiles.Items.Add(jsonFile)
+
+            Dim workoutList() = oJsonRoot.note.workouts
+            If workoutList.Count < 1 Then
+                Exit For    'ワークアウトが無い場合は無視
+            End If
+
+            'テンプレート ファイルを読込み、TCXファイルの出力用DOMオブジェクト
+            Dim domOut As XmlDocument = ReadDom(strTemplateFileName)
+            Dim elmActivity As XmlElement = domOut.GetElementsByTagName("Activity")(0)
+            Dim ndlLaps As XmlNodeList = elmActivity.GetElementsByTagName("Lap")
+
+            For Each workouts As workoutList In workoutList
+
+                strWorkoutMeter = workouts.meter
+                strWorkoutSec = workouts.sec
+                strGpxFilename = strGpxFolderName + workouts.route
+
+                Dim lapsList() As lapsList = workouts.laps
+
+                If Not File.Exists(strGpxFilename) Then
+                    Exit For 'GPXファイルが無い場合は無視
+                End If
+
+                'リストBOXへファイル名を追記
+                LbxGpxFiles.Items.Add(strGpxFilename)
+
+                'GPX XMLファイルをDOMオブジェクトに読込む
+                Dim domGpx As XmlDocument = ReadDom(strGpxFilename)
+                Dim ndlTrkPoints As XmlNodeList = domGpx.GetElementsByTagName("trkpt")
+
+                '開始時刻
+                Dim elmWorkoutStartTime As XmlElement = ndlTrkPoints(0)
+                Dim dtWorkoutStartTime As DateTime = System.TimeZoneInfo.ConvertTimeToUtc(elmWorkoutStartTime.GetElementsByTagName("time")(0).InnerText)
+
+                '経過時間
+                Dim iElapsedTime As Int32 = 0
+                Dim iLapNum As Int32 = 0
+                Dim iLapDistance As Int32
+                Dim iLapTime As Int32
+                Dim nmLapSpeed As Double = 0.0
+
+                Dim iTrNumPoints As Int32 = 0
+
+                'LAPデータ
+                Dim elmNewLap As XmlElement
+                Dim elmNewTracks As XmlElement
+                Dim elmNewTrackPoint As XmlElement
+
+                Dim strStartTime As String
+
+                '***** 各GPXポイントを処理 *****
+                For Each elmTrkPoint As XmlElement In ndlTrkPoints
+                    Dim strLat As String = elmTrkPoint.GetAttribute("lat")
+                    Dim strLon As String = elmTrkPoint.GetAttribute("lon")
+                    Dim dtGpsPoint As DateTime = System.TimeZoneInfo.ConvertTimeToUtc(elmTrkPoint.GetElementsByTagName("time")(0).InnerText)
+
+                    Dim tsElapsedTime As New TimeSpan(0, 0, 0, iElapsedTime)
+
+                    '***** LAP区切りの処理 *****
+                    If ((dtWorkoutStartTime + tsElapsedTime) <= dtGpsPoint) And (iLapNum <= lapsList.Count) Then
+                        'LAP開始時間
+                        Dim dtLapStartTime As DateTime = dtGpsPoint
+                        Dim strLapStartTime As String
+
+                        '***** LAP区間時間・距離の計算 *****
+                        If iLapNum < lapsList.Count Then
+                            '経過時間にLAP時間を加算
+                            iElapsedTime += CInt(lapsList(iLapNum).sec)
+
+                            '最終ラップを超えるまでは区間距離（今回ラップ距離-前回ラップ距離）
+                            If 0 < iLapNum Then
+                                iLapDistance = lapsList(iLapNum).meter - lapsList(iLapNum - 1).meter
+                            Else
+                                iLapDistance = lapsList(iLapNum).meter
+                            End If
+                            '時間はラップ時間のまま代入
+                            iLapTime = lapsList(iLapNum).sec
+
+                        Else '最終ラップ分が記録されていないので、最終ラップデータから計算する
+                            '最終ラップを超えたら、総走行距離から引く
+                            iLapDistance = CInt(strWorkoutMeter) - lapsList(iLapNum - 1).meter
+                            '時間はトータル時間から、経過時間　を差し引く
+                            iLapTime = CInt(strWorkoutSec) - iElapsedTime
+                        End If
+
+                        nmLapSpeed = iLapDistance / iLapTime
+
+                        'DOMにLAPを追加
+                        elmNewLap = ndlLaps(0).CloneNode(True)
+                        elmActivity.AppendChild(elmNewLap)
+
+                        strLapStartTime = dtLapStartTime.ToString("yyyy-MM-dd\THH:mm:ss\.000Z")
+                        elmNewLap.SetAttribute("StartTime", strLapStartTime)
+                        If iLapNum < 1 Then
+                            strStartTime = strLapStartTime
+                        End If
+
+                        elmNewLap.GetElementsByTagName("DistanceMeters")(0).InnerText = iLapDistance
+                        elmNewLap.GetElementsByTagName("TotalTimeSeconds")(0).InnerText = iLapTime
+                        elmNewLap.GetElementsByTagName("ns3:AvgSpeed")(0).InnerText = nmLapSpeed
+
+                        'LAPに追加するGPSデータツリー
+                        elmNewTracks = elmNewLap.GetElementsByTagName("Track")(0)
+
+                        TbxMessage.Text += "[LAP-" + (iLapNum + 1).ToString + "] " + iLapDistance.ToString + "m/" + iLapTime.ToString + "s" + vbCrLf
+
+                        iLapNum += 1
+
+                        iTrNumPoints = 0
+
+                    End If
+
+                    elmNewTrackPoint = elmNewTracks.GetElementsByTagName("Trackpoint")(0).CloneNode(True)
+                    elmNewTrackPoint.GetElementsByTagName("Time")(0).InnerText = dtGpsPoint.ToString("yyyy-MM-dd\THH:mm:ss\.000Z")
+                    elmNewTrackPoint.GetElementsByTagName("LatitudeDegrees")(0).InnerText = strLat
+                    elmNewTrackPoint.GetElementsByTagName("LongitudeDegrees")(0).InnerText = strLon
+
+                    elmNewTracks.AppendChild(elmNewTrackPoint)
+
+                    If iTrNumPoints < 1 Then
+                        elmNewTracks.RemoveChild(elmNewTracks.GetElementsByTagName("Trackpoint")(0))
+                    End If
+
+                    iTrNumPoints += 1
+
+                Next    'For Each elmTrkPoint
+
+                elmActivity.RemoveChild(elmActivity.GetElementsByTagName("Lap")(0))
+                elmActivity.GetElementsByTagName("Id")(0).InnerText = strStartTime
+
+                Dim strTcxFileName As String = strTcxFolderName + workouts.route.Replace(".gpx", ".tcx")
+
+                'DOMをファイルに出力
+                domOut.Save(strTcxFileName)
+
+                'リストBOXへファイル名を追記
+                LbxTcxFiles.Items.Add(strTcxFileName)
+
+            Next    'For Each workouts
+        Next    'For Each jsonFile
+    End Sub
+
+    Private Function ReadDom(file) As XmlDocument
+
+        ' GPX XMLファイルを読込むDOMオブジェクト
+        Dim dom As XmlDocument
+        ' XMLテンプレートドキュメントを初期化
+        dom = New XmlDocument
+        ' XMLテンプレートファイルを読み込み
+        dom.Load(file)
+
+        Return dom
+
+    End Function
+
+
+    'jsonファイルを読み込み、指定オブジェクトクラスに返す関数
+    Private Function ReadJson(Of T)(ByVal jsonFile As String) As T
+
+        Dim result As T
+        Dim streamJson As String = File.ReadAllText(jsonFile, Encoding.UTF8)
+        Dim serializer As New DataContractJsonSerializer(GetType(noteRoot))
+
+        Using stream As New IO.MemoryStream(Encoding.UTF8.GetBytes(streamJson))
+            result = DirectCast(serializer.ReadObject(stream), T)
+        End Using
+
+        Return result
+
+    End Function
+
+    Private Sub FrmGpxConvert_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub BtnExec_Click(sender As Object, e As EventArgs) Handles BtnExec.Click
+        Execute()
+    End Sub
+
+End Class
+
+Public Class noteRoot
+    Public Property note As noteList
+End Class
+
+Public Class noteList
+    Public Property note_id As String
+    Public Property [date] As String
+    Public Property weather As String
+    Public Property location As String
+    Public Property diary As String
+    Public Property physical_conditions As String
+    Public Property comments As String()
+    Public Property workouts As workoutList()
+End Class
+
+Public Class workoutList
+    Public Property workout_id As String
+    Public Property name As String
+    Public Property sec As String
+    Public Property meter As String
+    Public Property kcal As String
+    Public Property steps As String
+    Public Property memo As String
+    Public Property laps As lapsList()
+    Public Property route As String
+End Class
+
+Public Class lapsList
+    Public Property sec As String
+    Public Property meter As String
+End Class
